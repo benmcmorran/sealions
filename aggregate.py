988303cd7f7fcffa4d0883cc0f624e68d30ccb51
cjlovering <cjlovering@wpi.edu>
2017-04-30 17:35:55 +0000

    stash

--- aggregate.py
+++ aggregate.py
@@ -1,5 +1,8 @@
 import json
 import pandas
+import cv2
+import numpy as np
+from keras.models import load_model
 
 """
     Runs a model on sliced images and joins results
@@ -8,9 +11,9 @@ import pandas
     this will have to operate differently test vs train.
 """
 
-MODEL_PATH = 'model.h5'
-INSTANCES = 'test_instances.json'
-RESULTS = 'test_results.csv'
+MODEL_PATH = 'model_target_full_res.h5'
+INSTANCES = 'sliced_instances.json'
+RESULTS = 'train_results.csv'
 
 with open(INSTANCES) as in_f:
     instances = json.load(in_f)
@@ -23,21 +26,24 @@ model = load_model(MODEL_PATH)
 test_results = {}
 
 for img in images:
-    parent = img['original_id']
+    parent = img['file_name'].split('-')[0]
     file_name = img['file_name']
-    raw_img = cv2.imread('./data/Sliced/{0}'.format(file_name))
+    raw_img = cv2.imread('./data/SlicedTrain/{0}'.format(file_name))
     y = model.predict(np.array([raw_img]))
+    #print parent
     if parent in test_results:
         test_results[parent] += y
     else:
         test_results[parent] = y
-
+print test_results
 final_results = []
 for img, pop in test_results.iteritems():
+    print img
+    print pop
     record = [0, 0, 0, 0, 0, 0]
     record[0] = img
-    record[1:] = pop
+    record[1:] = pop[0]
     final_results.append(record)
 
-df = pandas.DataFrame(final_results)
+df = pandas.DataFrame(final_results, header=False, index=False)
 df.to_csv(RESULTS)
